7a830e463671cf9d4130070aa515d3842ef02ef4
FanWu <fwulunch@gmail.com>
2019-08-09 15:18:02 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config(object):
+    DEBUG = True
+    DEVELOPMENT = True
+    PATH_TO_FROZEN_GRAPH = 'path to frozenGraph.pb file'
+    PATH_TO_LABELS = 'path to labelmap.pbtxt file'
+    OBJ_PATH = 'tensorflow object detection directory'
+    OUT_PATH = 'output image directory'
